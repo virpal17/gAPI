5d6a5adb2fcc8b89c6763eb8f0a15cad229f6112
Virpal Kaur <virpalkaur170693@gmail.com>
2024-02-08 17:41:39 -0500

    add API

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/virpal17/gAPI
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// Message represent a simple JSON message
+type Message struct {
+	Greeting string `json:"greeting"`
+}
+
+func greetHandler(w http.ResponseWriter, r *http.Request) {
+	message := Message{Greeting: "Hello World!"}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(message)
+}
+
+func main() {
+	http.HandleFunc("/welcome", greetHandler)
+	http.ListenAndServe(":8011", nil)
+}
